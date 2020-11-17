edb4622650d000a960a16ec6ae9d2500b95081ef
Emily Watkins <emilyjaynewatkins@gmail.com>
2020-11-17 08:35:56 +0000

    adding star rating
    adding star rating across healthy-eating.js and all.designs.css

---
+++ healthy-eating.js
@@ -0,0 +1,39 @@
+var myRating = raterJs( {
+    element:document.querySelector("#rater"),
+    rateCallback:function rateCallback(rating, done) {
+      this.setRating(rating); 
+      done(); 
+    }
+});
+
+
+var myRating = raterJs({
+
+    // shows a rating tooltip
+    showToolTip: true,
+
+    // the number of stars
+    max: 5,
+
+    // star size
+    starSize: 16,
+
+    // text to show when disabled.
+    disableText: 'Thank you for your vote!',
+
+    // Text to show when hover over stars.
+    ratingText: '{rating}/{maxRating}',
+
+    // displayed while user is rating but done not called yet.
+    isBusyText: null,
+
+    // between 0 and 1
+    step: undefined,
+
+    // reverse the ratings
+    reverse: false,
+
+    // is readonly?
+    readOnly: false
+    
+});
